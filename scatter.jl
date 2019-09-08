e38786f1278b1814be39aaeadd1a3583b1bd3019
zhaotianjing <tjzhao@ucdavis.edu>
2019-09-08 15:43:04 -0700

    Add files via upload

---
+++ scatter.jl
@@ -0,0 +1,28 @@
+# EXAMPLE: scatter
+# here we have 3 ranks
+
+using MPI
+
+
+MPI.Init()
+comm = MPI.COMM_WORLD
+rank = MPI.Comm_rank(comm)
+
+
+if rank == 0
+    data = [7.0,8.0,9.0,10.0,11.0,12.0]
+else
+    data = Float64[]
+end
+
+data = MPI.Scatter(data, 2, 0, comm) #each rank has 2 elements
+@show rank, data
+
+
+MPI.Finalize()
+
+
+## output:
+# (rank, data) = (1, [9.0, 10.0])
+# (rank, data) = (2, [11.0, 12.0])
+# (rank, data) = (0, [7.0, 8.0])
